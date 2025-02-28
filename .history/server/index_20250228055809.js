c25e749280f30bff3f77d9759037fb00081dc4e8
makanda596 <149590359+makanda596@users.noreply.github.com>
2025-02-28 05:58:26 +0300

    S

---
+++ .history/server/index_20250228055809.js
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from 'mongoose'
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json()); // Parse JSON requests
+app.use(cors(
+    {
+        origin: ["https://thur-dior.vercel.app"],  // Specific frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,  // Allow cookies to be sent with the request
+})
+); // Enable Cross-Origin Resource Sharing
+
+// Basic route
+app.get("/a", (req, res) => {
+    res.send("Welcome to the backend server!");
+});
+
+// Simple login route
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    // Dummy user credentials
+    const user = { username: "admin", password: "password123" };
+
+    if (username === user.username && password === user.password) {
+        res.json({ message: "Login successful", status: "success" });
+    } else {
+        res.status(401).json({ message: "Invalid credentials", status: "error" });
+    }
+});
+
+const MONGO_URL = "mongodb+srv://oumab743:makandabrian123@cluster0.qj7my.mongodb.net/Gessamu?retryWrites=true&w=majority&appName=Cluster0"
+//mongo db connect
+mongoose.connect(MONGO_URL)
+try {
+    console.log("mongodb connected")
+}
+catch (error) {
+    console.error(error)
+}
+// Start the server
+app.listen(PORT, () => {
+    console.log('Server running on port 5000');
+});
